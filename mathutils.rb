c0f2708eb0c5fefe9c84abfc70bb9ff404a8dd1c
Tharq <bharanitharan170802@gmail.com>
2025-02-03 19:26:28 +0530

    Added MathUtils Homebrew formula

--- mathutils.rb
+++ mathutils.rb
@@ -1,8 +1,8 @@
 class Mathutils < Formula
     desc "A simple math utility library in C"
     homepage "https://github.com/tharq/mathutils"
-    url "https://github.com/tharq/mathutils/releases/download/v1.0.0/libmathutils.a"
-    sha256 "dafa7c82a89e98e5aa8b609fc697082d469f872f059a2e7139b0aef6d2475d7b"
+    url "https://github.com/tharq/mathutils/releases/download/v1.0.1/mathutils.tar.gz"
+    sha256 "your_sha256_hash"
   
     def install
       lib.install "libmathutils.a"
@@ -11,6 +11,8 @@ class Mathutils < Formula
   
     test do
       system "test -f #{lib}/libmathutils.a"
+      system "test -f #{include}/mathutils.h"
     end
   end
+  
   
